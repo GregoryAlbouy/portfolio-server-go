d18f83dd3ad5a4b7b3d0ae6ad54dd8a4318decc7
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-08-22 04:28:54 +0200

    fix PrefixPath

--- routes.go
+++ routes.go
@@ -2,5 +2,11 @@ package main
 
 func (s *server) routes() {
 	s.router.HandleFunc("/", s.handleIndex()).Methods("GET")
-	s.router.HandleFunc("/api/v1/projects/", s.handleProjectList())
+
+	pj := s.router.PathPrefix("/api/v1/projects/").Subrouter()
+	pj.HandleFunc("/test/", s.handleProjectList()).Methods("GET")
+	pj.HandleFunc("/{id:[0-9]+}", s.handleProjectDetail()).Methods("GET")
+	pj.HandleFunc("/", s.handleProjectCreation()).Methods("POST")
+	pj.HandleFunc("/{id:[0-9]+}", s.handleProjectUpdate()).Methods("PATCH")
+	pj.HandleFunc("/{id}", s.handleProjectDeletion()).Methods("DELETE")
 }
