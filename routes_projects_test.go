4d0bf4d0ea0b64625f54f75f2b4f0d74492c489f
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-08-25 15:19:16 +0200

    create unit & integration tests

--- routes_projects_test.go
+++ routes_projects_test.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,6 +18,26 @@ type testStore struct {
 	projectID int64
 	projects  []*Project
 }
+
+type testRequest struct {
+	method  string
+	target  string
+	headers map[string]string
+	body    io.Reader
+}
+
+var ct = map[string]string{
+	"json": "application/json",
+}
+
+var path = map[string]string{
+	"auth":    "/auth/",
+	"project": "/api/v1/projects/",
+}
+
+var srv = newTestServer()
+var testProject = validProject()
+var w = httptest.NewRecorder()
 
 func (t testStore) Open() error {
 	return nil
@@ -62,69 +86,128 @@ func (t testStore) Drop() error {
 }
 
 func TestCreateProjectUnit(t *testing.T) {
-	const (
-		method = "POST"
-		url    = "/api/v1/projects/"
-	)
-
-	srv := newServer()
-	srv.store = &testStore{}
-
-	p := map[string]string{
-		"name":        "Test project",
-		"slug":        "test-project",
-		"description": "This is a test project.",
-	}
-
-	payload := &bytes.Buffer{}
-	writer := multipart.NewWriter(payload)
-
-	for k, v := range p {
-		err := writer.WriteField(k, v)
-		assert.Nil(t, err)
-	}
-
-	err := writer.Close()
+	fd, ctype, err := createFormData(testProject)
 	assert.Nil(t, err)
 
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/api/v1/projects/", payload)
-	r.Header.Set("Content-Type", writer.FormDataContentType())
+	r := (&testRequest{
+		method: "POST",
+		target: path["project"],
+		headers: map[string]string{
+			"Content-Type": ctype,
+		},
+		body: fd,
+	}).Set()
 
 	srv.createProject()(w, r)
 	assert.Equal(t, http.StatusCreated, w.Code)
 }
 
+func testAuthRoute(t *testing.T) {
+
+}
+
 func TestCreateProjectIntegration(t *testing.T) {
+	fd, ctype, err := createFormData(testProject)
+	assert.Nil(t, err)
+
+	token, err := validToken()
+	assert.Nil(t, err)
+
+	r := (&testRequest{
+		method: "POST",
+		target: path["project"],
+		headers: map[string]string{
+			"Authorization": authHeader(token),
+			"Content-Type":  ctype,
+		},
+		body: fd,
+	}).Set()
+
+	srv.router.ServeHTTP(w, r)
+	assert.Equal(t, http.StatusCreated, w.Code)
+}
+
+func credentialsPayload() (io.Reader, error) {
+	b, err := json.Marshal(User{
+		Username: os.Getenv("AUTH_USERNAME"),
+		Password: os.Getenv("AUTH_PASSWORD"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes.NewReader(b), nil
+}
+
+func validToken() (string, error) {
 	const (
-		method = "POST"
-		url    = "/api/v1/projects/"
+		URL   = "http://localhost:8080/auth/"
+		ctype = "application/json"
 	)
 
+	type responseToken struct {
+		Token string `json:"token"`
+	}
+	var rt responseToken
+
+	payload, err := credentialsPayload()
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.Post(URL, ctype, payload)
+	if err != nil {
+		return "", err
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&rt); err != nil {
+		return "", err
+	}
+
+	return rt.Token, nil
+}
+
+func authHeader(token string) string {
+	return fmt.Sprintf("Bearer %s", token)
+}
+
+func createFormData(m map[string]string) (*bytes.Buffer, string, error) {
+	fd := &bytes.Buffer{}
+	w := multipart.NewWriter(fd)
+
+	for k, v := range m {
+		if err := w.WriteField(k, v); err != nil {
+			return nil, "", err
+		}
+	}
+
+	if err := w.Close(); err != nil {
+		return nil, "", err
+	}
+
+	return fd, w.FormDataContentType(), nil
+}
+
+func newTestServer() *server {
 	srv := newServer()
 	srv.store = &testStore{}
+	return srv
+}
 
-	p := map[string]string{
+func validProject() map[string]string {
+	return map[string]string{
 		"name":        "Test project",
 		"slug":        "test-project",
 		"description": "This is a test project.",
 	}
+}
 
-	payload := &bytes.Buffer{}
-	writer := multipart.NewWriter(payload)
+func (tr *testRequest) Set() *http.Request {
+	r := httptest.NewRequest(tr.method, tr.target, tr.body)
 
-	for k, v := range p {
-		err := writer.WriteField(k, v)
-		assert.Nil(t, err)
+	for k, v := range tr.headers {
+		r.Header.Set(k, v)
 	}
 
-	err := writer.Close()
-	assert.Nil(t, err)
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/api/v1/projects/", payload)
-	r.Header.Set("Content-Type", writer.FormDataContentType())
-
-	srv.router.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusCreated, w.Code)
+	return r
 }
